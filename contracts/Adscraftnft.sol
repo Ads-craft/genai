882950285004da80ed4ede2988a53fc103dcd3bc
Sikander Ali <71672759+sikandermit@users.noreply.github.com>
2023-12-07 05:39:03 +0500

    update full stack

---
+++ contracts/Adscraftnft.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+contract Adscraftnft is ERC721URIStorage, Ownable {
+    string constant TOKEN_URI =
+        "https://ipfs.io/ipfs/";
+    uint256 internal tokenId;
+
+    constructor() ERC721("Adscraftnft", "AcNFT") {}
+
+    function mint(address to) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, TOKEN_URI);
+        unchecked {
+            tokenId++;
+        }
+    }
+}
